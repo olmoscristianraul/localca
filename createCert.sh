7c9541fdc53300d0a25c51c895b95569adc962e3
Facundo Perez <facundoperez94@gmail.com>
2020-07-19 20:21:29 -0300

    Update createCert.sh

--- createCert.sh
+++ createCert.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-read -p "Enter Common Name (Site Name, eg. something.test.home): " domainName
-read -p "Enter CA FileName: " caFileName
+read -p "Ingresar nombre del dominio (Site Name, eg. odoo.example.com): " domainName
+read -p "Ingresar nombre del CA (HCSinergiaCA): " caFileName
 openssl genrsa -out ${domainName}.key 2048
 
-openssl req -new -key ${domainName}.key -subj "/C=US/ST=Arizona/L=Phoenix/CN=${domainName}" -out ${domainName}.csr
+openssl req -new -key ${domainName}.key -subj "/C=AR/ST=Buenos Aires/L=La Plata/CN=${domainName}" -out ${domainName}.csr
 
 cat <<EOF >> ${domainName}.cnf
 authorityKeyIdentifier=keyid,issuer
@@ -14,13 +14,11 @@ subjectAltName = @alt_names
 DNS.1 = ${domainName}
 EOF
 
-
-
 #openssl v1.1  > supports this below
-#openssl x509 -req -in ${domainName}.csr -CA ${caFileName}.pem -CAkey ${caFileName}.key -CAcreateserial -out ${domainName}.crt -days 1825 -sha256 -addext "subjectAltName=DNS:${domainName}"
+#openssl x509 -req -in ${domainName}.csr -CA ${caFileName}.pem -CAkey ${caFileName}.key -CAcreateserial -out ${domainName}.crt -days 365 -sha256 -addext "subjectAltName=DNS:${domainName}"
 #but for openssl < 1.1
 stty -echo 
-openssl x509 -req -in ${domainName}.csr -CA ${caFileName}.pem -CAkey ${caFileName}.key -CAcreateserial -out ${domainName}.crt -days 1825 -sha256 -extfile ${domainName}.cnf
+openssl x509 -req -in ${domainName}.csr -CA ${caFileName}.pem -CAkey ${caFileName}.key -CAcreateserial -out ${domainName}.crt -days 365 -sha256 -extfile ${domainName}.cnf
 stty echo 
 #cleanup
 rm ${domainName}.cnf
